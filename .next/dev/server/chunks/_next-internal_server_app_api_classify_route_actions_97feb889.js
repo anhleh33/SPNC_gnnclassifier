cee4d1bed208806066774dc6e5227de2989f55f9
QC2k4 <quang987xyz@gmail.com>
2025-12-12 21:03:33 +0700

    Remove .next from tracking

--- .next/dev/server/chunks/_next-internal_server_app_api_classify_route_actions_97feb889.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/classify/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_classify_route_actions_97feb889.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"N36HXsIxNJy1nFb1PcwkFIqygymLiI+lwHN3bsCe+2s=\"\n}"
