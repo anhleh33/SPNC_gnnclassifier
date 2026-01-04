69cb3436835be6b1a78f7304f1ab0fb86ecb81ae
QC2k4 <quang987xyz@gmail.com>
2026-01-04 21:57:56 +0700

    [REFACTORED] main.app

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,28 @@
 from flask import Flask
-from backend.presentation.routes.default_routes import default_bp
-from backend.presentation.routes.user_routes import user_bp
-from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS
 from dotenv import load_dotenv
-import os
+
+from backend.settings import SHOW_STARTUP_MESSAGE, DOCS_URL, SWAGGER_URL
+from backend.presentation.routes.default_routes import default_bp
+from backend.presentation.routes.user_routes import user_bp
+from backend.presentation.routes.swagger_routes import swagger_bp
 
 load_dotenv()
 
-
 def create_app():
     app = Flask(__name__)
+
+    app.logger.setLevel("INFO")
 
     CORS(app)
 
     app.register_blueprint(default_bp)
     app.register_blueprint(user_bp, url_prefix="/users")
-
-     # Swagger UI
-    SWAGGER_URL = "/docs"
-    API_URL = "/static/openapi.yaml"
-
-    swagger_bp = get_swaggerui_blueprint(
-        SWAGGER_URL,
-        API_URL,
-        config={
-            "app_name": "GNN Classifier API"
-        }
-    )
-
     app.register_blueprint(swagger_bp, url_prefix=SWAGGER_URL)
+
+    # ✅ Startup message (modern Flask way)
+    if SHOW_STARTUP_MESSAGE and DOCS_URL:
+        app.logger.info("OpenAPI docs available at %s", DOCS_URL)
 
     return app
 
--- backend/settings.py
+++ backend/settings.py
@@ -7,3 +7,15 @@ load_dotenv()
 # Database
 # ─────────────────────────────
 DATABASE_URL: str = os.getenv("DATABASE_URL")
+
+# ─────────────────────────────
+# Logger Configuration
+# ─────────────────────────────
+SHOW_STARTUP_MESSAGE: bool = True
+DOCS_URL: str = "http://127.0.0.1:5000/docs"
+
+# ─────────────────────────────
+# Swagger Configuration
+# ─────────────────────────────
+SWAGGER_URL: str = "/docs"
+API_URL: str = "/static/openapi.yaml"
