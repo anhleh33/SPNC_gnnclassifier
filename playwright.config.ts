8ef7c41cda288de292226c1519407ed1e42d0de2
Anh Le Hoang <anhlehoang2004@gmail.com>
2026-01-04 21:52:03 +0700

    [RE-CONFIG]

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined, // Keep at 1 to avoid DB collisions
-  reporter: [['html'], ['allure-report']],
+  reporter: [['html']],
   
   use: {
     trace: 'on-first-retry',
